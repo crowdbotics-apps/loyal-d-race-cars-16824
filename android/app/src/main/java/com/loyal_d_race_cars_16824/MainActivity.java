67adfd575c78884e9f86804cdfc96558bb8553bd
Crowdbotics <team@crowdbotics.com>
2020-05-10 04:11:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/loyal_d_race_cars_16824/MainActivity.java
@@ -0,0 +1,15 @@
+package com.loyal_d_race_cars_16824;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "loyal_d_race_cars_16824";
+    }
+}
